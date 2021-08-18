7d7092976f5ade1812104bbfdd48785044b27e9e
dah-romy <romeorazaf@gmail.com>
2021-08-18 22:14:02 +0300

    Installation

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+    Sgomez\Bundle\BotmanBundle\BotmanBundle::class => ['all' => true],
 ];
