ebdddc8c02bc6f0d52ab129acd8cd59734f41039
dah-romy <romeorazaf@gmail.com>
2021-08-18 22:52:49 +0300

    Bot controller

---
+++ src/Controller/WebhookController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the `botman-demo` project.
+ *
+ * (c) Sergio Gómez <sergio@uco.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use BotMan\BotMan\BotMan;
+use Symfony\Component\HttpFoundation\Response;
+
+class WebhookController
+{
+    public function __invoke(BotMan $botman): Response
+    {
+        $botman->fallback(function (BotMan $bot): void {
+            $bot->reply($bot->getMessage()->getText());
+        });
+
+        $botman->listen();
+
+        return new Response('', Response::HTTP_OK);
+    }
+}
