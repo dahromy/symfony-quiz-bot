6a4a56b38e7fc3944de579432f02bfbc5fb6b198
dah-romy <romeorazaf@gmail.com>
2021-08-18 22:17:55 +0300

    Symfony profiler

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Sgomez\Bundle\BotmanBundle\BotmanBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
