ebb470dde55df9b8966a72c69c5a860af28740b1
dah-romy <romeorazaf@gmail.com>
2021-08-18 22:34:07 +0300

    Update

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,5 @@ return [
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Sgomez\Bundle\BotmanBundle\BotmanBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
